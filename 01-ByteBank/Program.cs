f422554ef353c98ceccc0fe4fe4c4136acfb4b85
Leandro Roberto <leandrobaccan76@gmail.com>
2021-08-17 22:34:17 -0300

    csharp aula classes 2

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,7 +10,25 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100.00;
+            
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência número: " + contaDaGabriela.agencia);
+            Console.WriteLine("Conta Corrente número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo da Conta: R$ " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo Atual da Conta: R$ " + contaDaGabriela.saldo);
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ 02 - ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+
+            Console.WriteLine(conta.titular);
+
+
+            Console.ReadLine();
+        }
+    }
+}
